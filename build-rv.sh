eba0b2af4c6aac840a00affef7ae56bef59f617b
Trần Văn Cầu <rosino999@gmail.com>
2023-02-28 15:32:12 +0700

    fix bug 7

--- build-rv.sh
+++ build-rv.sh
@@ -100,7 +100,7 @@ java -jar revanced-cli*.jar -a *youtube*.apk -b revanced-patches*.jar -m revance
 # Find and select apksigner binary
 apksigner="$(find $ANDROID_SDK_ROOT/build-tools -name apksigner | sort -r | head -n 1)"
 # Sign apks (https://github.com/tytydraco/public-keystore)
-${apksigner} sign --ks public.jks --ks-key-alias public --ks-pass pass:public --key-pass pass:public --in ./revanced.apk --out ./yt-${NAME}-v${VERSION}.apk
+${apksigner} sign --ks public.jks --ks-key-alias public --ks-pass pass:public --key-pass pass:public --in ./revanced-${USER}.apk --out ./yt-${NAME}-v${VERSION}.apk
 
 
 
